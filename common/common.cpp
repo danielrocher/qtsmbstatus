c857e6707ff42267c10bd1ed90af73f64b539134
Daniel Rocher <daniel.rocher@adella.org>
2008-01-02 22:28:26 +0000

    Version 2.0.3 (very minor release)

--- common/common.cpp
+++ common/common.cpp
@@ -38,9 +38,9 @@
 
 
 // variables environnements
-QString version_qtsmbstatus="2.0.2";
-uint int_qtsmbstatus_version=202;
-QString date_qtsmbstatus="2007-11-14";
+QString version_qtsmbstatus="2.0.3";
+uint int_qtsmbstatus_version=203;
+QString date_qtsmbstatus="2008-01-02";
 QString auteur_qtsmbstatus="Daniel Rocher";
 QString mail_qtsmbstatus="<a href='mailto:daniel.rocher@adella.org'>daniel.rocher@adella.org</a>";
 QString web_qtsmbstatus="<a href='http://qtsmbstatus.free.fr'>http://qtsmbstatus.free.fr</a>";
