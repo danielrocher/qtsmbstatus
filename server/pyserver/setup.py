5dfdff33899f7405bd33a75e96511111c70eda14
Daniel Rocher <daniel.rocher@adella.org>
2012-12-04 22:10:15 +0100

    add post-install in setup.py (install data files, ...)

--- server/pyserver/setup.py
+++ server/pyserver/setup.py
@@ -3,7 +3,17 @@
 
 
 from distutils.core import setup
+from distutils.command import install
+import os, sys
 
+
+class custom_install(install.install):
+    def run(self):
+        install.install.run(self)
+        print("Run postinstall ...")
+        pathname = format(os.path.dirname(sys.argv[0]))
+        if os.system("{}/../postinst.sh".format(os.path.abspath(pathname)))!=0:
+            print("postinstall failed !")
 
 setup(name='qtsmbstatus_pyserver',
       version='2.3.0',
@@ -15,6 +25,10 @@ setup(name='qtsmbstatus_pyserver',
       url='http://qtsmbstatus.free.fr',
       download_url='http://qtsmbstatus.free.fr/index.php?page=downloads',
       packages=['qtsmbstatus_pyserver'],
-      scripts=['qtsmbstatusd']
+      scripts=['qtsmbstatusd'],
+      data_files=[('/etc/pam.d', ['../pam.d/qtsmbstatusd']),
+                  ('/etc/qtsmbstatusd', ['../etc/privkey.pem', '../etc/qtsmbstatusd.conf', '../etc/qtsmbstatusd.users', '../etc/server.pem']),
+                  ('/etc/init.d', ['../etc/qtsmbstatusd'])],
+      cmdclass={ 'install': custom_install }
       )
 
